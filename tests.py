8cffa166b494d15740c168cdc3c511ce33ef57cc
Ahmad Raza Jamal <33037741+AhmadRazaJamal@users.noreply.github.com>
2021-05-24 16:48:32 -0500

    renamed sorting filename and added test

--- tests.py
+++ tests.py
@@ -2,10 +2,11 @@ import unittest
 import random
 from selection_sort import selection_sort
 from quick_sort import quick_sort
+from merge_sort import merge_sort
+
 import sys
 
 sys.setrecursionlimit(1500)
-
 
 num_array = [i for i in range(1000)]
 
@@ -18,15 +19,23 @@ num_array2 = num_array.copy()
 sorted_array2 = num_array2
 sorted_array2.sort()
 
+num_array3 = num_array.copy()
+sorted_array3 = num_array3
+sorted_array3.sort()
+
+
 class SimpleTest(unittest.TestCase):
-  
-    #tests output of selection_sort against output of python's inbuilt sort. 
-    def test_selection_sort(self):        
-        self.assertEqual(sorted_array,  selection_sort(num_array) )
+
+    # tests output of selection_sort against output of python's inbuilt sort.
+    def test_selection_sort(self):
+        self.assertEqual(sorted_array, selection_sort(num_array))
 
     def test_quick_sort(self):
-        self.assertEqual(sorted_array2,  quick_sort(num_array2))
+        self.assertEqual(sorted_array2, quick_sort(num_array2))
+
+    def test_merge_sort(self):
+        self.assertEqual(sorted_array3, quick_sort(num_array3))
+
 
 if __name__ == '__main__':
     unittest.main()
-    
